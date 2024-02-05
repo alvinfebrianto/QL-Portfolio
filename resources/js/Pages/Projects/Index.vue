49f4ae77b14377d54c26942c3e56db8a8c56de2c
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-02-05 16:35:55 +0700

    -

---
+++ resources/js/Pages/Projects/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Head title="Skills Index" />
+  <BreezeAuthenticatedLayout>
+      <template #header>
+          <h2 class="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>
+      </template>
+
+      <div class="py-12">
+          <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">Table</div>
+      </div>
+  </BreezeAuthenticatedLayout>
+</template>
+
+<script setup>
+  import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+  import { Head } from '@inertiajs/inertia-vue3';
+</script>
